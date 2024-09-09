7d0ee5f64e1da64e9bd3e0f1d7ce36a777329089
angelbelangel5 <nglbelangel@gmail.com>
2024-09-09 16:12:54 +0800

    modify the user routes by adding  the login routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/signup", userController.createUser);
-
+router.post("/login", userController.loginUser);
 module.exports = router;
