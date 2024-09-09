154ba266f428b87cb592f7b6037c230ecf80d70c
angelbelangel5 <nglbelangel@gmail.com>
2024-09-09 15:56:29 +0800

    Add user routes and update index.js to include userRoutes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todoRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 const cors = require("cors");
 
 require("dotenv").config();
@@ -15,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI);
 app.use(bodyParser.json());
 
 app.use("/todo", todoRoutes);
+app.use("/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
