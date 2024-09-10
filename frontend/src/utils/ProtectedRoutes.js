d738b5360be17b232a81c636b0497aa86a677352
angelbelangel5 <nglbelangel@gmail.com>
2024-09-10 15:12:57 +0800

    add protected routes that only authentiocated user can access

---
+++ frontend/src/utils/ProtectedRoutes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoutes = () => {
+  const token = localStorage.getItem('token'); 
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoutes;
