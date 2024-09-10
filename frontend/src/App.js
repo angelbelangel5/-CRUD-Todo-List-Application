0b77c8140910826ec1c55b015a0f1e6437ff8c1f
angelbelangel5 <nglbelangel@gmail.com>
2024-09-10 14:13:23 +0800

    add login and signup route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TodoList from './components/TodoList';
-import AddTodo from './components/AddTodo'
-
+import SignUp from './components/SignUp'
+import Login from './components/Login';
+import AddTodo from './components/AddTodo';
 
 const App = () => {
   return (
@@ -11,7 +12,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<TodoList />} />
         <Route path="/add" element={<AddTodo />} />
-       
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+
       </Routes>
     </Router>
   );
